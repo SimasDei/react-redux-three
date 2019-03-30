d17bddae5e4081589d30460ffe314de8046c5f74
SimasDei <simondeiman@gmail.com>
2019-03-30 18:32:46 +0200

    add and remove esling :)

--- src/App.js
+++ src/App.js
@@ -4,27 +4,16 @@ import './App.css';
 
 import Movie from './Components/Movie';
 
-const movies = [
-  {
-    id: 1,
-    title: 'Star Wars'
-  },
-  {
-    id: 2,
-    title: 'Spider Man'
-  },
-  {
-    id: 3,
-    title: 'Seven'
-  }
-];
-
 const API = {
   url: 'https://api.themoviedb.org/3/',
   key: 'ab1108ff64d84d869773eb7692b0749f'
 };
 
 class App extends Component {
+  state = {
+    movies: []
+  };
+
   async componentDidMount() {
     try {
       const result = await fetch(
@@ -33,13 +22,14 @@ class App extends Component {
         }&language=en-US&sort_by=popularity.desc&page=1`
       );
       const movies = await result.json();
-      console.log(movies);
+      this.setState({ movies: movies.results });
     } catch (error) {
       console.error(error);
     }
   }
 
   render() {
+    const { movies } = this.state;
     return (
       <div className="App">
         <header className="App-header">
--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 export class Movie extends Component {
-  static propTypes = {
-    movie: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      description: PropTypes.string
-    })
-  };
-
-  static defaultProps = {};
-
   render() {
     const { movie } = this.props;
     return (
