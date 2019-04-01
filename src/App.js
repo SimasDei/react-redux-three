64a55a68351ada5f49f6d5eddd7b4558600cd843
SimasDei <simondeiman@gmail.com>
2019-04-01 16:19:14 +0300

    Add reducer, root reducer, setup store

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {createStore} from 'redux'
-import {Provider} from 'react-redux';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './rootReducer';
 import './App.css';
 
 import Header from './Components/Header';
 import MoviesList from './Components/MoviesList';
 import MovieDetails from './Components/MovieDetails';
 
+const store = createStore(rootReducer);
 
 class App extends Component {
   render() {
     return (
-      <Provider store={}>
-      <Router>
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route path="/" exact component={MoviesList} />
-            <Route path={'/:id'} component={MovieDetails} />
-          </Switch>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Switch>
+              <Route path="/" exact component={MoviesList} />
+              <Route path={'/:id'} component={MovieDetails} />
+            </Switch>
+          </div>
+        </Router>
       </Provider>
     );
   }
--- src/reducer.js
+++ src/reducer.js
@@ -2,4 +2,13 @@ const initialState = {
   messageVisibility: false
 };
 
-export default function(state = initialState, action) {}
+export default function(state = initialState, action) {
+  const { type } = action;
+  switch (type) {
+    case 'TOGGLE_MESSAGE':
+      return state;
+
+    default:
+      return state;
+  }
+}
